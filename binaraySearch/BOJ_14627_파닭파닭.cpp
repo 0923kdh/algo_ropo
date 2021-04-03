3a199b97867dc874a05c52a911ad368defb904d0
0923kdh <0923kdh@gmail.com>
2021-04-04 00:13:17 +0900

    BOJ_14627_파닭파닭

---
+++ binaraySearch/BOJ_14627_파닭파닭.cpp
@@ -0,0 +1,49 @@
+﻿#include<stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+ll s, c, cnt, sum, arr[1000000];
+
+bool check(ll mid)
+{
+	ll quotient = 0;
+	for (int i = 0; i < s; i++)
+		quotient += arr[i] / mid;
+
+	return quotient < c;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> s >> c;
+	ll low = 0, high = 0;
+	for (int i = 0; i < s; i++)
+	{
+		cin >> arr[i];
+		sum += arr[i];
+		high = max(high, arr[i]);
+	}
+
+	while (low <= high)
+	{
+		ll mid = (low + high) / (1LL * 2);
+		if (check(mid))
+		{
+			high = mid - 1;
+		}
+		else
+		{
+			cnt = mid;
+			low = mid + 1;
+		}
+	}
+
+	cout << sum - (cnt * c) << "\n";
+	return 0;
+}
+
