ad4ae2c3fcbfcb72aebf8ca2589c58d3391482f5
0923kdh <0923kdh@gmail.com>
2021-04-08 01:01:13 +0900

    프로그래머스_괄호변환 success

---
+++ simulation/프로그래머스_괄호변환.cpp
@@ -0,0 +1,70 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+bool check(string str)
+{
+    int cnt = 0;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] == '(')
+            cnt++;
+        else
+            cnt--;
+
+        if (cnt < 0)
+            return false;
+    }
+
+    return true;
+}
+
+string tranformString(string str)
+{
+    if (str.empty())
+        return "";
+
+    string u, v;
+    int left = 0, right = 0;
+    for (int i = 0; i < str.size(); i++)
+    {
+        char c = str[i];
+        if (c == '(')
+            left++;
+        else
+            right++;
+
+        if (left == right)
+        {
+            u = str.substr(0, i + 1);
+            v = str.substr(i + 1);
+            break;
+        }
+    }
+
+    if (check(u))
+        return u + tranformString(v);
+    else
+    {
+        string temp = "(";
+        temp += tranformString(v) + ")";
+        u = u.substr(1, u.size() - 2);
+        for (int i = 0; i < u.size(); i++)
+        {
+            if (u[i] == '(')
+                temp += ')';
+            else
+                temp += '(';
+        }
+        return temp;
+    }
+}
+
+string solution(string p)
+{
+    if (check(p))
+        return p;
+
+    return tranformString(p);
+}
