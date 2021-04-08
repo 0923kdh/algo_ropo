e57275a48256fb222f395bb4f72c133869720384
0923kdh <0923kdh@gmail.com>
2021-04-09 00:06:54 +0900

    프로그래머스_캐쉬 success

---
+++ simulation/프로그래머스_캐쉬.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities)
+{
+    if (cacheSize == 0)
+        return 5 * cities.size();
+
+    vector<string> cache;
+    int answer = 0;
+    for (int i = 0; i < cities.size(); i++)
+    {
+        //소문자 변환
+        transform(cities[i].begin(), cities[i].end(), cities[i].begin(), ::tolower);
+
+        auto iter = find(cache.begin(), cache.end(), cities[i]);
+
+        //cache miss
+        if (iter == cache.end())
+        {
+            if (cache.size() == cacheSize)
+                cache.erase(cache.begin());
+
+            answer += 5;
+        }
+        //cache hit
+        else
+        {
+            cache.erase(iter);
+            answer += 1;
+        }
+
+        cache.push_back(cities[i]);
+    }
+
+    return answer;
+}
