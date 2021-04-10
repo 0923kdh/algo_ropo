7da343f5e88fe868763251affc325daf673da678
0923kdh <0923kdh@gmail.com>
2021-04-11 00:16:36 +0900

    BOJ_15650_N과M_2 success

---
+++ backtracking/BOJ_15650_N과M_2.cpp
@@ -0,0 +1,40 @@
+﻿#include<bis/stdc++.h>
+
+using namespace std;
+
+int n, m, arr[9], visited[9];
+
+void dfs(int num, int next)
+{
+	if (num == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = next; i <= n; i++)
+	{
+		if (visited[i])
+			continue;
+
+		visited[i] = 1;
+		arr[num] = i;
+		dfs(num + 1, i);
+		visited[i] = 0;
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	dfs(0, 1);
+	return 0;
+}
