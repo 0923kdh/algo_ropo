21f3a52c06a56f3d7f5539b489ba9940a75e56da
0923kdh <0923kdh@gmail.com>
2021-04-16 01:22:00 +0900

    BOJ_4781_사탕가게 success

---
+++ dp/BOJ_4781_사탕가게.cpp
@@ -0,0 +1,40 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, c, dp[10001];
+double m, p;
+pair<int, int> candy[5001];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (true)
+	{
+		memset(dp, 0, sizeof(dp));
+		cin >> n >> m;
+		if (n == 0)
+			break;
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> c >> p;
+			candy[i] = make_pair(c, (int)(p * 100 + 0.5));
+		}
+
+		int money = (int)(m * 100 + 0.5);
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = candy[i].second; j <= money; j++)
+			{
+				dp[j] = max(dp[j], dp[j - candy[i].second] + candy[i].first);
+			}
+		}
+		cout << dp[money] << "\n";
+	}
+
+	return 0;
+}
