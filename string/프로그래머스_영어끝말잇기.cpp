d6c868164cd86abb950d1770a6c24caa37233071
0923kdh <0923kdh@gmail.com>
2021-04-26 23:48:57 +0900

    프로그래머스_영어끝말잇기 success

---
+++ string/프로그래머스_영어끝말잇기.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(int n, vector<string> words)
+{
+    vector<int> answer;
+    set<string> s;
+    char prev = words[0].front();
+    int people = 0, cnt = 0;
+    for (int i = 0; i < words.size(); i++)
+    {
+        auto iter = s.find(words[i]);
+        if (iter == s.end() && prev == words[i].front())
+        {
+            s.insert(words[i]);
+            prev = words[i].back();
+            continue;
+        }
+
+        people = (i % n) + 1;
+        cnt = i / n + 1;
+        break;
+    }
+
+    answer.push_back(people);
+    answer.push_back(cnt);
+    return answer;
+}
