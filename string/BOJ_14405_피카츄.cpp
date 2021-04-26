9d9e33b14beea5b42e3ed6faa022a61f33512ff2
0923kdh <0923kdh@gmail.com>
2021-04-27 00:40:31 +0900

    BOJ_14405_피카츄 success

---
+++ string/BOJ_14405_피카츄.cpp
@@ -0,0 +1,33 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+string s;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> s;
+	bool isSucceed = true;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (i < s.size() - 1 && (s.substr(i, 2) == "pi" || s.substr(i, 2) == "ka"))
+			i += 1;
+		else if (i < s.size() - 2 && (s.substr(i, 3) == "chu"))
+			i += 2;
+		else
+		{
+			isSucceed = false;
+			break;
+		}
+	}
+
+	if (isSucceed)
+		cout << "YES" << "\n";
+	else
+		cout << "NO" << "\n";
+	return 0;
+}
