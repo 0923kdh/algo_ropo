c2e9fd5254a4f411b951f1aee0ab3588deac3c1b
0923kdh <0923kdh@gmail.com>
2021-04-30 00:34:48 +0900

    프로그래머스_다리를지나는트럭 success

---
+++ simulation/프로그래머스_다리를지나는트럭.cpp
@@ -0,0 +1,41 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights)
+{
+    queue<int> q;
+    int time = 0;
+    int tempTotalWeight = 0;
+    int truckIndex = 0;
+
+    while (true)
+    {
+        time++;
+
+        if (q.size() == bridge_length)
+        {
+            tempTotalWeight -= q.front();
+            q.pop();
+        }
+
+        if (tempTotalWeight + truck_weights[truckIndex] <= weight)
+        {
+            if (truckIndex == truck_weights.size() - 1)
+            {
+                time += bridge_length;
+                break;
+            }
+
+            q.push(truck_weights[truckIndex]);
+            tempTotalWeight += truck_weights[truckIndex];
+            truckIndex++;
+        }
+        else
+        {
+            q.push(0);
+        }
+    }
+
+    return time;
+}
