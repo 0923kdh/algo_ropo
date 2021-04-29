0053bba7bc2f56132dbf9214e44f8205c00124e4
0923kdh <0923kdh@gmail.com>
2021-04-29 22:53:15 +0900

    BOJ_9012_괄호 success

---
+++ string/BOJ_9012_괄호.cpp
@@ -0,0 +1,54 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+int n;
+string str;
+vector<string> v;
+
+bool check(string str)
+{
+    stack<char> s;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] == '(')
+        {
+            s.push(str[i]);
+        }
+        else
+        {
+            if (s.empty() == false)
+                s.pop();
+            else
+            {
+                // )인데 (가 없을때
+                return false;
+            }
+        }
+    }
+    //stack에 괄호 남아있을때
+    return s.empty();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> str;
+        if (check(str))
+            v.push_back("YES");
+        else
+            v.push_back("NO");
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << "\n";
+    }
+
+    return 0;
+}
