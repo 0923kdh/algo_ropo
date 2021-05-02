0a628c769427e05380861c4fd41b53b775f8da03
0923kdh <0923kdh@gmail.com>
2021-05-02 22:34:10 +0900

    프로그래머스_카펫 success

---
+++ bruteForce/프로그래머스_카펫.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) 
+{
+    vector<int> answer;
+    int sum = brown + yellow;
+    for (int height = 3; height <= sqrt(sum); height++)
+    {
+        if ((sum % height) == 0)
+        {
+            int width = sum / height;
+            if (sum - (width - 2) * (height - 2) == brown)
+            {
+                answer.push_back(width);
+                answer.push_back(height);
+
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
