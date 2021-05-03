d7765c200b7d8c9cbb1a996cf5b27af9799bdcac
0923kdh <0923kdh@gmail.com>
2021-05-04 01:37:55 +0900

    프로그래머스_타겟넘버 success

---
+++ dfs/프로그래머스_타겟넘버.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int answer, sum;
+vector<int> v;
+
+void dfs(int target, int index)
+{
+    if (index == v.size())
+    {
+        if (sum == target)
+        {
+            answer++;
+        }
+        return;
+    }
+
+    dfs(target + v[index], index + 1);
+    dfs(target - v[index], index + 1);
+}
+
+int solution(vector<int> numbers, int target)
+{
+    sum = target;
+    v = move(numbers);
+    dfs(0, 0);
+    return answer;
+}
