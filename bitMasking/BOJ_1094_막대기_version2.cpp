6a2bcac3d4a362bfdf38c04c91e5aa94c4cb248e
0923kdh <0923kdh@gmail.com>
2021-05-05 22:56:36 +0900

    BOJ_1094_막대기_version2 success

---
+++ bitMasking/BOJ_1094_막대기_version2.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int x, sum, barLength = 64, cnt;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> x;
+	while (x != sum)
+	{
+		if ((sum + barLength) <= x)
+		{
+			sum += barLength;
+			cnt++;
+		}
+		barLength /= 2;
+	}
+	cout << cnt << '\n';
+	return 0;
+}
