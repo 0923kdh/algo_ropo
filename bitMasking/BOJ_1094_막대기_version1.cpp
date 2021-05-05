fb3992580aa1ba53f04f3e4bd15901f3e63f5b27
0923kdh <0923kdh@gmail.com>
2021-05-05 22:56:08 +0900

    BOJ_1094_막대기_version1 success

---
+++ bitMasking/BOJ_1094_막대기_version1.cpp
@@ -0,0 +1,16 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	cout << bitset<8>(n).count() << '\n';
+	return 0;
+}
