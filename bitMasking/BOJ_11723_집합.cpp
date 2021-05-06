da4de961deeedde6bb8b55624cf9775f532c35a6
0923kdh <0923kdh@gmail.com>
2021-05-06 21:17:00 +0900

    BOJ_11723_집합 success

---
+++ bitMasking/BOJ_11723_집합.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int m, n, num;
+char str[15];
+
+int main()
+{
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%s %d", &str, &num);
+        if (str[0] == 'a' && str[1] == 'd') n |= (1 << num);
+        else if (str[0] == 'r') n &= ~(1 << num);
+        else if (str[0] == 'c') cout << ((n & (1 << num)) == 0 ? 0 : 1) << '\n';
+        else if (str[0] == 't') n ^= (1 << num);
+        else if (str[0] == 'a' && str[1] == 'l') n = (1 << 21) - 1;
+        else n = 0;
+    }
+
+    return 0;
+}
