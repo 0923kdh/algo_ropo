b2b909c9bc8f957c75c6237c0088267236bbb097
0923kdh <0923kdh@gmail.com>
2021-05-15 01:57:55 +0900

    BOJ_4375_1 success

---
+++ math/BOJ_4375_1.cpp
@@ -0,0 +1,27 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+	//ios_base::sync_with_stdio(false);
+	//cin.tie(NULL);
+	//cout.tie(NULL);
+	ll n = 0.0;
+	while (cin >> n)
+	{
+		ll m = 1.0;
+		int digits = 1;
+		while ((m % n) != 0)
+		{
+			m %= n;
+			m = m * 10 + 1;
+			//m = (m * 10) % n + 1;
+			digits++;
+		}
+		cout << digits << "\n";
+	}
+
+	return 0;
+}
