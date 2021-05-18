d66c165c55e36afe95bb5c25805a0629ba2953f2
0923kdh <0923kdh@gmail.com>
2021-05-19 00:33:00 +0900

    프로그래머스_2개이하로다른비트 succeed

---
+++ bitMasking/프로그래머스_2개이하로다른비트.cpp
@@ -0,0 +1,29 @@
+﻿#include <bitset>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) 
+{
+    vector<long long> answer;
+    for(int i = 0; i < numbers.size(); i++)
+    {
+        bitset s = bitset<52>(numbers[i]);
+        for(int j = 0; j < s.size(); j++)
+        {
+            if(s[j] == 0)
+            {
+                s[j] = 1;
+                if(j != 0)
+                {
+                    s[j-1] = 0;
+                }
+                break;
+            }   
+        }
+        answer.push_back(s.to_ulong());
+    }
+    
+    return answer;
+}
