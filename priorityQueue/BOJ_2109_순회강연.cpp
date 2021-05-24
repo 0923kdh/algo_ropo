7b53e22ff61c7013eba68c9707e5b01fa2ec0194
0923kdh <0923kdh@gmail.com>
2021-05-24 22:48:46 +0900

    BOJ_2109_순회강연 성공

---
+++ priorityQueue/BOJ_2109_순회강연.cpp
@@ -0,0 +1,43 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, d, p, ret;
+vector<pair<int, int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> d >> p;
+        //일, 강연료
+        v.push_back({ p,d });
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        pq.push(v[i].second);
+        if (pq.size() > v[i].first)
+        {
+            pq.pop();
+        }
+    }
+
+    while (pq.size())
+    {
+        ret += pq.top();
+        pq.pop();
+    }
+
+    cout << ret << "\n";
+
+    return 0;
+}
