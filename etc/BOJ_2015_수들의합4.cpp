60b42579dbbc4c6de714c1834d92f533d025f039
0923kdh <0923kdh@gmail.com>
2021-05-27 17:29:23 +0900

    BOJ_2015_수들의합4 성공

---
+++ etc/BOJ_2015_수들의합4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int n, m, arr[200001], pSum[200001];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    vector<int> v(n+1);
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        pSum[i] = pSum[i-1] + arr[i];
+    }
+
+    map<int, int> cnt;
+    cnt[0] = 1;
+    long long ans = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        //arr[j] + ... + arr[i] == s[i] - s[j-1]
+        //pSum[i] - s[j-1] == m
+        //s[j-1] = s[i] - m (j - 1 < i)
+        ans += cnt[pSum[i]-m];
+        cnt[pSum[i]] += 1;
+    }
+    
+    cout << ans << '\n';
+
+    return 0;
+}
