a3021677125ae23b234a7a60330cb18c34d74007
0923kdh <0923kdh@gmail.com>
2021-06-01 00:35:51 +0900

    프로그래머스 짝지어 제거하기 성공

---
+++ stack/프로그래머스_짝지어제거하기.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> stk;
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(stk.size() && (stk.top() == s[i]))
+            stk.pop();            
+        else if((i < s.size() - 1) && s[i] == s[i+1])
+            i += 1;
+        else
+            stk.push(s[i]);        
+    }
+    
+    return !stk.size();
+}
