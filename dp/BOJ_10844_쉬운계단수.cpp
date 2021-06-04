9fd34556a5a8f1103b189682dc7705f9969824df
0923kdh <0923kdh@gmail.com>
2021-06-04 16:02:32 +0900

    BOJ_10844_쉬운계단수 성공

---
+++ dp/BOJ_10844_쉬운계단수.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+ll ans, dp[101][11];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);                                                                                                                                                                 
+
+    cin >> n;
+    dp[1][0] = 0;
+    for(int i = 1; i <= 9; i++)
+        dp[1][i] = 1;
+
+    for(int i = 2; i <= n; i++)
+    {
+        for(int j = 0; j <= 9; j++)
+        {
+            if(j == 0)
+                dp[i][j] = dp[i-1][j+1] % 1000000000;
+            else if(j == 9)
+                dp[i][j] = dp[i-1][j-1] % 1000000000;
+            else
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000;
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+        ans = ans + dp[n][i];
+
+    cout << ans % 1000000000;
+
+    return 0;
+}
