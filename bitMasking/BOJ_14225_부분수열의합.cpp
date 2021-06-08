8e1e9c9a66d285bac6e101a69acb3d5967902273
0923kdh <0923kdh@gmail.com>
2021-06-09 01:06:31 +0900

    BOJ 14225 부분수열의 합 성공

---
+++ bitMasking/BOJ_14225_부분수열의합.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n, a[21], visited[20*100000+2];
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for(int i = 1; i < (1 << n); i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < n; j++)
+        {
+            if(i & (1 << j))
+                sum += a[j];
+        }
+        visited[sum] = 1;
+    }
+
+    for(int i = 1; i < 20*100000+3; i++)
+    {
+        if(visited[i] == 0)
+        {
+            cout << i << "\n";
+            break;
+        }
+    }
+
+	return 0;
+}
