c801510a718565143743c0dbd6da2c5271c07253
0923kdh <0923kdh@gmail.com>
2021-06-10 20:27:20 +0900

    프로그래머스 보석쇼핑 성공

---
+++ twoPointers/프로그래머스_보석쇼핑.cpp
@@ -0,0 +1,51 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<string> gems) 
+{
+    vector<int> answer;
+    map<string, int> m;
+    set<string> s(gems.begin(), gems.end());
+    
+    int min = 0, start = 0, end = 0;
+    for(const auto& gem : gems)
+    {
+        m[gem]++;
+        if(m.size() == s.size())
+            break;
+        end++;
+    }
+    
+    min = end - start;
+    answer.push_back(start+1);
+    answer.push_back(end+1);
+    
+    while(end < gems.size())
+    {
+        string jewName = gems[start];
+        m[jewName]--;
+        start++;
+        
+        if(m[jewName] == 0)
+        {
+            end++;
+            for(int i = end; i < gems.size(); i++)
+            {
+                m[gems[i]]++;
+                if(jewName == gems[i])
+                    break;
+                end++;
+            }
+        }
+
+        if(min > end - start)
+        {
+            answer[0] = start + 1;
+            answer[1] = end + 1;
+            min = end - start;
+        }
+    }
+    
+    return answer;
+}
