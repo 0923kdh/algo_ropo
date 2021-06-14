430671d5ea440b70d90b124e5a77c72b73f3adfb
0923kdh <0923kdh@gmail.com>
2021-06-14 22:49:48 +0900

    BOJ 7795 먹을 것인가 먹힐 것인가 성공

---
+++ binaraySearch/BOJ_7795_먹을것인가먹힐것인가.cpp
@@ -0,0 +1,48 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int t, n, m, num;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> t;
+    vector<int> ansList;
+    while (t--)
+    {
+        cin >> n >> m;
+     
+        vector<int> aList, bList;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> num;
+            aList.push_back(num);
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            cin >> num;
+            bList.push_back(num);
+        }
+
+        sort(bList.begin(), bList.end());
+
+        int count = 0;
+        for (const auto& a : aList)
+        {
+            count += lower_bound(bList.begin(), bList.end(), a) - bList.begin();
+        }
+        ansList.push_back(count);
+    }
+
+    for (const auto& ans : ansList)
+    {
+        cout << ans << "\n";
+    }
+ 
+    return 0;
+}
