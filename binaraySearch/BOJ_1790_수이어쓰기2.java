d1727ee01ddc3f21033504c91d1dbc9b4c16fb1d
0923kdh <0923kdh@gmail.com>
2021-06-16 20:44:55 +0900

    BOJ 1790 수 이어 쓰기 2 성공

---
+++ binaraySearch/BOJ_1790_수이어쓰기2.java
@@ -0,0 +1,51 @@
+﻿import java.util.*;
+
+public class test {
+    static long go(int num)
+    {
+        long ans = 0;
+        int len = 1;
+        for(int start = 1; start <= num; start *= 10)
+        {
+            int end = start * 10 - 1;
+            if(end > num)
+                end = num;
+            ans += (long)(end - start + 1) * len;
+            len++;
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long k = sc.nextLong();
+
+        if(go(n) < k)
+        {
+            System.out.println(-1);
+            System.exit(0);
+        }
+
+        int left = 1;
+        int right = n;
+        int ans = 0;
+        while(left <= right)
+        {
+            int mid = (left + right)/2;
+            if(go(mid) < k)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                ans = mid;
+                right = mid - 1;
+            }
+        }
+
+        String s = Integer.toString(ans);
+        long l = go(ans);
+        System.out.println(s.charAt(s.length()-(int)(l-k)-1));
+    }
+}
