a2d39668cb666a31824246e07be0172bae8e4081
0923kdh <0923kdh@gmail.com>
2021-06-17 14:43:41 +0900

    BOJ 2110 공유기 설치 성공

---
+++ binaraySearch/BOJ_2110_공유기설치.java
@@ -0,0 +1,47 @@
+﻿import java.util.*;
+
+public class Main {
+
+    static boolean check(int mid, int c, int[] arr)
+    {
+        int cnt = 1;
+        int last = arr[0];
+        for(int house : arr)
+        {
+            if(house - last >= mid)
+            {
+                cnt+=1;
+                last = house;
+            }
+        }
+        return cnt >= c;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+
+        int ans = 0, lo = 1, hi = a[n-1] - a[0];
+        while(lo <= hi)
+        {
+            int mid = (lo + hi) / 2;
+            if(check(mid, c, a.clone()))
+            {
+                ans = mid;
+                lo = mid + 1;
+            }
+            else
+            {
+                hi = mid - 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
