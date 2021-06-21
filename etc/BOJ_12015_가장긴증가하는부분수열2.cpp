39405f52808cf0024e92cd677b5ab01e235671af
0923kdh <0923kdh@gmail.com>
2021-06-21 23:17:15 +0900

    BOJ 12015 가장 긴 증가하는 부분수열2

---
+++ etc/BOJ_12015_가장긴증가하는부분수열2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<int> v;
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        auto it = lower_bound(v.begin(), v.end(), num);
+        if(it == v.end()) {
+            v.push_back(num);
+        } else {
+            *it = num;
+        }
+    }
+
+    cout << v.size() << "\n";
+
+	return 0;
+}
