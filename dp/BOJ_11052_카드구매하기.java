dbdfa341caae1d89f85c83d9cbb705f240b45687
0923kdh <0923kdh@gmail.com>
2021-06-27 22:26:54 +0900

    BOJ 11052 카드구매하기 성공

---
+++ dp/BOJ_11052_카드구매하기.java
@@ -0,0 +1,22 @@
+﻿import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n+1];
+		int[] dp = new int[n+1];
+
+		for(int i = 1; i <=n; i++) {
+			a[i] = sc.nextInt();
+		}
+
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) {
+				dp[i] = Math.max(dp[i], dp[i-j]+a[j]);
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}
