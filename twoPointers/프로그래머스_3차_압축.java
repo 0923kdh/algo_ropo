e90497f1bac2cbb758a3680d26944aa62b830b9e
0923kdh <0923kdh@gmail.com>
2021-07-05 18:47:46 +0900

    프로그래머스_3차_압축 성공

---
+++ twoPointers/프로그래머스_3차_압축.java
@@ -0,0 +1,46 @@
+﻿import java.util.*;
+
+class Solution {
+    
+	static HashMap<String, Integer> map = new HashMap<>();
+	static ArrayList<Integer> list = new ArrayList<>();
+	
+	public static void init() {
+		for(int i = 0; i < 26; i++) {
+			map.put(Character.toString('A' + i), i+1);
+		}
+	}
+    
+    public int[] solution(String msg) {
+		init();
+		int value = 27;
+		boolean isEnd = false;
+		String preWord ="";
+		for(int i = 0; i < msg.length(); i++) {
+			String word = Character.toString(msg.charAt(i));
+			while(map.containsKey(word)) {
+				i++;
+				preWord = word;
+				if(i == msg.length()) {
+					isEnd = true;
+					break;
+				}
+				word += Character.toString(msg.charAt(i));
+			}
+			
+			list.add(map.get(preWord));
+			if(isEnd) {
+				break;
+			}
+			map.put(word, value++);
+			i--;
+		}
+
+		int[] answer = new int[list.size()];
+		for(int i = 0; i < answer.length; i++) {
+			answer[i] = list.get(i);
+		}
+        
+        return answer;
+    }
+}
