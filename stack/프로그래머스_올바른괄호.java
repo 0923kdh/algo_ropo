ef403214a63ebe94dc01efadee4a36f5a9e4f759
0923kdh <0923kdh@gmail.com>
2021-07-08 17:12:04 +0900

    프로그래머스_올바른괄호 성공

---
+++ stack/프로그래머스_올바른괄호.java
@@ -0,0 +1,23 @@
+﻿import java.util.*;
+
+class Solution {
+    
+    boolean solution(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.add('(');
+            } else {
+                if(stack.empty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        
+        return stack.empty();
+    }
+}
