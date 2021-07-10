5dbb3835f3c2fee43cf339a80dbae79a0755efbd
0923kdh <0923kdh@gmail.com>
2021-07-10 21:57:53 +0900

    프로그래머스_가장큰정사각형찾기 성공

---
+++ dp/프로그래머스_가장큰정사각형찾기.java
@@ -0,0 +1,26 @@
+﻿import java.util.*;
+
+class Solution
+{   
+    public int solution(int [][]board) {
+        int answer = 0, row = board.length, col = board[0].length;     
+        int[][] arr = new int [row+1][col+1];
+        
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                arr[i+1][j+1] = board[i][j];
+            }
+        }
+        
+        for(int i = 1; i <= row; i++) {
+            for(int j = 1; j <= col; j++) {
+                if(arr[i][j] != 0) {
+                    arr[i][j] = Math.min(Math.min(arr[i][j-1], arr[i-1][j]), arr[i-1][j-1]) + 1;
+                    answer = Math.max(answer, arr[i][j]);
+                }
+            }
+        }
+        
+        return answer * answer;
+    }   
+}
