0ad864c8847dad50c20700c3d00330d1c40bad6a
0923kdh <0923kdh@gmail.com>
2021-07-21 22:18:24 +0900

    프로그래머스_숫자문자열과영단어 성공

---
+++ string/프로그래머스_숫자문자열과영단어.java
@@ -0,0 +1,10 @@
+﻿class Solution {
+    public int solution(String s) {
+        String[] alpha = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+ 
+        for(int i = 0; i <= 9; i++) {
+            s = s.replaceAll(alpha[i], String.valueOf(i));
+        }
+        return Integer.parseInt(s);
+    }
+}
