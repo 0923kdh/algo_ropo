d137143d3634f08e82338e81c4f7197ea8aa4016
0923kdh <0923kdh@gmail.com>
2021-07-23 17:38:09 +0900

    프로그래머스_징검다리 성공

---
+++ binaraySearch/프로그래머스_징검다리.cpp
@@ -0,0 +1,43 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int deleteRockCnt;
+vector<int> temp;
+
+bool check(int mid) {
+    
+    int cnt = 0, tempValue = temp[0];
+    for(int i = 1; i < temp.size(); i++) {
+        int dis = temp[i] - tempValue;
+        if(mid <= dis) {
+            tempValue = temp[i];
+        } else {
+            cnt++;
+        }
+    }
+
+    return deleteRockCnt < cnt;
+}
+
+int solution(int distance, vector<int> rocks, int n) {
+    
+    deleteRockCnt = n;
+    temp = rocks;
+    temp.push_back(0);
+    temp.push_back(distance);
+    sort(temp.begin(), temp.end());
+ 
+    int ans = 0, lo = 0, hi = temp[temp.size()-1];
+    while(lo <= hi) {
+        int mid = (lo + hi) / 2;
+        if(check(mid)) {
+            hi = mid - 1;    
+            ans = hi;
+        } else {   
+            lo = mid + 1;
+        }      
+    }
+    
+    return ans;
+}
