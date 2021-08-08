73b511de7a19b18e8ad3a8dfcfe89db609ac33a5
0923kdh <0923kdh@gmail.com>
2021-08-08 23:08:22 +0900

    BOJ_2493_탑 성공

---
+++ stack/BOJ_2493_탑.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int n, num, ans;
+stack<pair<int, int>> s; //num, index
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cin >> num;
+
+        if(i == 1) {
+            s.push({num, i});
+            cout << ans << " ";
+            continue;
+        } 
+
+        if(s.top().first < num) {
+            while(s.size()) {
+                if(s.top().first > num) {
+                    ans = s.top().second;
+                    break;
+                }                    
+                s.pop();
+            }
+        } else {
+            if(s.size()) {
+                ans = s.top().second;
+            }
+        }
+
+        if(s.empty()) {
+            ans = 0;
+        }
+       
+        cout << ans << " ";
+
+        s.push({num ,i});
+    }
+   
+    return 0;
+}
