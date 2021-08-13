89bd937078daf058df766ddb09a511b407ba35f7
0923kdh <0923kdh@gmail.com>
2021-08-13 21:42:41 +0900

    leetcode1769 성공

---
+++ etc/leetcode1769.java
@@ -0,0 +1,22 @@
+﻿class Solution {
+    public int[] minOperations(String boxes) {
+        int boxesSize = boxes.length();
+        int[] ans = new int[boxesSize];
+
+        int count = 0, totalCount = 0;
+        for(int i = 0; i < boxesSize; i++) {
+            ans[i] += totalCount;
+            count += boxes.charAt(i) == '1' ? 1 : 0;
+            totalCount += count;
+        }
+
+        count = 0;
+        totalCount = 0;
+        for(int i = boxesSize -1; i >= 0; i--) {
+            ans[i] += totalCount;
+            count += boxes.charAt(i) == '1' ? 1 : 0;
+            totalCount += count;
+        }
+        return ans;
+    }
+}
