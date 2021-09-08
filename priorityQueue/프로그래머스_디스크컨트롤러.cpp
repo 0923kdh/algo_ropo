0d66d3c74b0370ae6dd84d271ce73603dc9c68e7
0923kdh <0923kdh@gmail.com>
2021-09-08 15:25:25 +0900

    프로그래머스 디스크컨트롤러 성공

---
+++ priorityQueue/프로그래머스_디스크컨트롤러.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<vector<int>> jobs) {
+    int answer = 0, index = 0, time = 0;
+    sort(jobs.begin(), jobs.end());
+    priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+    while(index < jobs.size() || !pq.empty()) {
+        if(jobs.size() > index && time >= jobs[index][0]){
+            pq.push({jobs[index][1], jobs[index][0]});
+            index++;
+            continue;
+        }
+
+        if(!pq.empty()){
+            vector<int> temp = pq.top();
+            time += temp[0];
+            answer += time - temp[1];
+            pq.pop();
+        }else{
+            time = jobs[index][0];
+        }
+    }
+    return answer / jobs.size();
+}
