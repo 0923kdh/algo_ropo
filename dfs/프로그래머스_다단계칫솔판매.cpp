b865ca07446cc91b6233c8264cf8d0763c5e940f
0923kdh <0923kdh@gmail.com>
2021-10-07 15:59:24 +0900

    프로그래머스 다단계칫솔판매 성공

---
+++ dfs/프로그래머스_다단계칫솔판매.cpp
@@ -0,0 +1,37 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+map<string, string> parent;
+map<string, int> profit;
+
+void calculateProfit(string name, int money) {
+    if(name == "-")
+        return;
+    
+    int parentProfit = money * 0.1;
+    profit[name] += (money - parentProfit);    
+    if(parentProfit < 1)
+        return;
+    
+    calculateProfit(parent[name], parentProfit);   
+}
+
+vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {    
+    for(int i = 0; i< enroll.size(); i++) {
+        parent[enroll[i]] = referral[i];
+    }
+    
+    for(int i = 0; i < seller.size(); i++) {
+        calculateProfit(seller[i], amount[i]*100);
+    }
+    
+    vector<int> answer;
+    for(int i = 0; i< enroll.size(); i++) {
+        answer.push_back(profit[enroll[i]]);
+    }
+    
+    return answer;
+}
