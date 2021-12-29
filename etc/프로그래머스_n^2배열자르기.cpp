d2332261526df29e10ce10b2aaf3e80a0c26a98f
0923kdh <0923kdh@gmail.com>
2021-12-29 15:32:21 +0900

    프로그래머스 n^2배열자르기 성공

---
+++ etc/프로그래머스_n^2배열자르기.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    for (long long i = left; i <= right; ++i) {
+        long long quotient = i / n;
+        long long remainder = i % n;
+        if(quotient >= remainder){
+            answer.push_back(quotient+1);
+        }else{
+            long long gap = remainder - quotient;
+            answer.push_back(quotient+1+gap);
+        }
+    }
+    return answer;
+}
