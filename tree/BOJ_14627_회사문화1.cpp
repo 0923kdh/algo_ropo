7f74fe149f3d7f15c28ccea8b8576625136561f7
0923kdh <0923kdh@gmail.com>
2022-01-04 18:29:08 +0900

    BOJ_14627 회사문화1 성공

---
+++ tree/BOJ_14627_회사문화1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m, elder, compliment[100001], people, w;
+vector<int> adj[100001];
+
+void go(int startVertex, int amount){
+    for(auto next : adj[startVertex]){
+        compliment[next] += amount;
+        go(next, compliment[next]);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i) {
+        cin >> elder;
+        adj[elder].push_back(i);
+    }
+
+    for (int i = 0; i < m; ++i) {
+        cin >> people >> w;
+        compliment[people] += w;
+    }
+
+    go(1, compliment[1]);
+
+    for(int i=1; i <= n; i++){
+        cout << compliment[i] << " " ;
+    }
+
+    return 0;
+}
