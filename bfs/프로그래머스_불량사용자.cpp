c00712d26e1b49a12d84c8349b8fb1d76b5a3145
0923kdh <0923kdh@gmail.com>
2022-01-05 18:59:24 +0900

    프로그래머스_불량사용자 성공

---
+++ bfs/프로그래머스_불량사용자.cpp
@@ -0,0 +1,43 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int check[9];
+set<string> id;
+
+void dfs(vector<string> userId, vector<string> bannedId, int index){
+    if(index == bannedId.size()){
+        string str="";
+        for(int i = 0; i < userId.size(); i++){
+            if(check[i])
+                str += (i+'0');
+        }
+        id.insert(str);
+        return;
+    }
+
+    for(int i = 0; i < userId.size(); i++){
+        if(check[i] || bannedId[index].size() != userId[i].size())
+            continue;
+
+        bool isMatching = true;
+        for(int j = 0; j < userId[i].size(); j++){
+            if(bannedId[index][j] != '*' && bannedId[index][j] != userId[i][j]){
+                isMatching = false;
+                break;
+            }
+        }
+
+        if(isMatching){
+            check[i] = 1;
+            dfs(userId, bannedId, index+1);
+            check[i] = 0;
+        }
+    }
+}
+
+int solution(vector<string> user_id, vector<string> banned_id) {
+    dfs(user_id, banned_id, 0);
+
+    return id.size();
+}
