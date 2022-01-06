34d3f6a783acbd1827774e4f7359f7d18d2bd953
0923kdh <0923kdh@gmail.com>
2022-01-06 20:10:42 +0900

    BOJ 17073 나무위의빗물 성공

---
+++ tree/BOJ_17073_나무위의빗물.cpp
@@ -0,0 +1,43 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int cnt, visited[500001];
+vector<int> adj[500001];
+
+void go(int vertex){
+    bool isLeafNode = true;
+    for(int i = 0; i < adj[vertex].size(); i++) {
+        int nextVertex = adj[vertex][i];
+        if (visited[nextVertex])
+            continue;
+
+        visited[nextVertex] = 1;
+        isLeafNode = false;
+        go(nextVertex);
+    }
+
+    if(isLeafNode)
+        cnt++;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(11);
+
+    int n, u, v;
+    double w;
+    cin >> n >> w;
+    for (int i = 0; i < n - 1; i++) {
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    visited[1] = 1;
+    go(1);
+    cout << w / cnt;
+    return 0;
+}
