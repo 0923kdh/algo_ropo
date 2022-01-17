1670e6ca70ff637af1c21b2b321529c6a1d62a8d
0923kdh <0923kdh@gmail.com>
2022-01-17 22:29:19 +0900

    BOJ 1300 K번째수 성공

---
+++ binaraySearch/BOJ_1300_K번째수.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+
+bool check(int mid){
+    int cnt = 0;
+    for(int i = 1; i <= n; i++){
+        cnt += min(mid / i, n);
+    }
+
+    return cnt < k;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> k;
+    int ans = 0, low = 0, high = k;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(check(mid)){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+            ans = mid;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
